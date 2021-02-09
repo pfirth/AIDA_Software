7abfc11c108c5ba3c5da17252c754bdf1e4a02e0
Peter <peter.firth@asu.edu>
2021-02-08 17:08:06 -0700

    Updated to work @ ASU for SC3000

--- main.py
+++ main.py
@@ -652,7 +652,7 @@ GB2 = goBetween(MainScreen,gateValve,PLC,MFCList,BaratronList,RFGenList,StepperC
 LoadingScreen = LoadSamplePopUpLogic(ParameterDictionary,PLC,StepperController)
 sm.add_widget(LoadingScreen)
 
-class TestApp(App):
+class SC300App(App):
     def on_start(self):
         print("App Starting")
         self.loadSettings()
@@ -679,7 +679,7 @@ class TestApp(App):
 
 
 if __name__ == '__main__':
-        TestApp().run()
+        SC300App().run()
 
 
 
